877269d997019388207f546051d1a8cce8598d4f
a20160098 <131293237+a20160098@users.noreply.github.com>
2023-09-13 12:14:48 -0500

    Add files via upload

---
+++ cuentas.js
@@ -0,0 +1,18 @@
+var entrada = parseInt(prompt("Ingrese el monto de la entrada"));
+var segundo = parseInt(prompt("Ingrese el monto del segundo plato"));
+var postre = parseInt(prompt("Ingrese el monto del postre"));
+
+var montoparcial = entrada + segundo + postre;
+
+var igv = montoparcial + 18 / 100;
+
+var montototal = montoparcial + igv;
+
+console.log("El monto de la entrada es: "+ entrada);
+console.log("El monto del segundo plato es: "+ segundo);
+console.log("El monto del postre es: "+ postre);
+
+console.log("El igv aciende a: "+ igv);
+
+console.log("El monto total a pagar es: "+ monto total;
+
